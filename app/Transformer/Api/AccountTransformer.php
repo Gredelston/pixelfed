0bd870e65e3724356c7cd2d4a34acba55eb7b343
Greg Edelston <gredelston@gmail.com>
2026-03-31 23:48:51 -0600

    Add email verification banner to homepage

--- app/Transformer/Api/AccountTransformer.php
+++ app/Transformer/Api/AccountTransformer.php
@@ -7,6 +7,7 @@ use App\Services\AccountService;
 use App\Services\PronounService;
 use App\User;
 use App\UserSetting;
+use Auth;
 use Cache;
 use League\Fractal;
 
@@ -73,6 +74,7 @@ class AccountTransformer extends Fractal\TransformerAbstract
             'last_fetched_at' => optional($profile->last_fetched_at)->toJSON(),
             'pronouns' => PronounService::get($profile->id),
             'location' => $profile->location,
+            'email_verified_at' => ($local && Auth::check() && $profile->user_id == Auth::id()) ? Auth::user()->email_verified_at : null,
         ];
 
         if ($profile->moved_to_profile_id) {
